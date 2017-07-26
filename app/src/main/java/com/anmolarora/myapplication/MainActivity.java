a3787d2a5bca80ede8471764d2c1efafdc4083a8
Anmol Arora <anmolsingharora@github.com>
2017-07-27 00:02:16 +0530

    UsingImages

---
+++ app/src/main/java/com/anmolarora/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.anmolarora.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void switchPicture(View view){
+
+        ImageView image = (ImageView) findViewById(R.id.batmanImage);
+        image.setImageResource(R.drawable.batman_logo);
+
+        Log.i("Test", "Button Clicked");
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
